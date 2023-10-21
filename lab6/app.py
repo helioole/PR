226207d2f3a0c52ee469fef3b80ffaae3611f442
helioole <irarakovcena@gmail.com>
2023-10-21 19:13:02 +0300

    modified

--- lab6/app.py
+++ lab6/app.py
@@ -2,11 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
 from models.database import db
 from models.electro_scooter import ElectroScooter 
+from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///scooter_database.db'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:@localhost/postgres'
+    migrate = Migrate(app, db)
     db.init_app(app)
     return app
 
